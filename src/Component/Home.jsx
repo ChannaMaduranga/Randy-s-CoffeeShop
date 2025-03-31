eb7e03b3308bac56a9dda1749ce1ff0cb7e78f7d
Channa Maduranga <83961725+ChannaMaduranga@users.noreply.github.com>
2025-04-01 02:03:59 +0530

    update home

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -5,12 +5,21 @@ import Menu from './Menu'
 import Gallery from './Gallery'
 import Feedback from './Feedback'
 import Contact from './Contact'
+import { FaArrowUp } from "react-icons/fa";
 
 
 function Home() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+   // Scroll to top function
+   const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", 
+    });
+  };
   return (
     <div>
         <div id='home'>
@@ -21,6 +30,11 @@ function Home() {
             <Feedback/>
             <Contact/>
         </div>
+        <button
+        onClick={scrollToTop}
+        className="fixed bottom-8 right-8 bg-[#dac5a7] text-gray-700 p-3 rounded-full shadow-lg hover:opacity-80">
+        <FaArrowUp size={16} />
+      </button>
     </div>
   )
 }
